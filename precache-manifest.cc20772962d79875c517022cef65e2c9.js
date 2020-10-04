9fd13c28271c38893697ae8378cbf6f335ed2621
sandeep <sandeep.gantait@sap.com>
2020-10-04 21:43:33 +0530

    Updates

---
+++ precache-manifest.cc20772962d79875c517022cef65e2c9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "48246a2476b782695be8d69dee66a8c8",
+    "url": "/treetable/index.html"
+  },
+  {
+    "revision": "46b590dfbc28d1b844d5",
+    "url": "/treetable/static/css/main.8afcf23c.chunk.css"
+  },
+  {
+    "revision": "a917be6654964322a707",
+    "url": "/treetable/static/js/2.77f9eb77.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/treetable/static/js/2.77f9eb77.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "46b590dfbc28d1b844d5",
+    "url": "/treetable/static/js/main.fad04055.chunk.js"
+  },
+  {
+    "revision": "efdbb2da8db33ee58337",
+    "url": "/treetable/static/js/runtime-main.43c7cf31.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/dev-sandeep/treetable/precache-manifest.6ce12e2d54bf15eae7b503c36390e29b.js"
+  "/treetable/precache-manifest.cc20772962d79875c517022cef65e2c9.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/dev-sandeep/treetable/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/treetable/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
